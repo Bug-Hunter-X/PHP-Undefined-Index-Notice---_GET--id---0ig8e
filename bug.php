67fa9b036d867dc5bb91d2bffd444d63f076ddd8
BugHunter-X <askzenai@gmail.com>
2024-12-18 00:48:04 +0100

    Add bug.php file

---
+++ bug.php
@@ -0,0 +1,1 @@
+This code attempts to access an array element using a non-existent key, leading to an undefined index notice or error.  The issue is that the `$_GET` array may not always contain the expected 'id' key.
